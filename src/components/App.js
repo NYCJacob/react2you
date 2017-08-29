32c74e1d902c16b2d1659219bb5a3ff60d4070c0
Jacob Sherman <jacob@jbsherman.com>
2017-08-29 17:44:06 -0400

    working on addCat reducer error

--- src/components/App.js
+++ src/components/App.js
@@ -9,57 +9,24 @@ import Posts from './Posts';
 
 class App extends Component {
 
-    state = {
-        commentApp: this.props.store.getState()
-        // commentApp: null
+
+
+    render() {
+        console.log('Props: ', this.props);
+
+        return (
+            <div className="App">
+                <div>
+                    <h1>Post, listen, react</h1>
+                    <Categories/>
+                </div>
+                <div>
+                    <Posts/>
+                </div>
+
+            </div>
+        );
     }
-
-    componentDidMount () {
-        const { store } = this.props;
-
-        store.subscribe(() => {
-            console.log( store.getState() );
-            this.setState(() => ({
-                commentApp: store.getState()
-            }))
-        })
-
-    }
-
-     // generate random integer for key index where no unique value available
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-     getRandomInt = (min = 1, max = 999999) =>  {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-
-    submitCategory = () => {
-        this.props.store.dispatch(addCategory({
-            name: this.input.value,
-            pathName : this.input.value,
-        }))
-
-        this.input.value = ''
-    }
-
-  render() {
-      console.log('Props: ', this.props);
-
-    return (
-      <div className="App">
-
-          <div>
-              <h1>Post, listen, react</h1>
-                <Categories/>
-          </div>
-          <div>
-              <Posts/>
-          </div>
-
-      </div>
-    );
-  }
 }
 
 export default App
