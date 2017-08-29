8fca0b50cec4550e6b90e904dec214eb24a45298
Jacob Sherman <jacob@jbsherman.com>
2017-08-29 17:57:16 -0400

    custom logger added BUG: undefinded error in addCategory switch in reducers

--- src/index.js
+++ src/index.js
@@ -4,15 +4,31 @@ import { Provider } from 'react-redux';
 import './styles/index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+
+import { createStore, applyMiddleware, compose } from 'redux'
+
 // import reducer from './reducers'
 
 import rootReducer from './reducers'
 
+const logger = store => next => action => {
+      console.group(action.type)
+      console.info('dispatching', action)
+      let result = next(action)
+          console.log('next state', store.getState())
+          console.groupEnd(action.type)
+          return result
+        }
+
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+
 //if redux reducer exists invoke redux devtools
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(logger)
+    )
 );
 
 ReactDOM.render(
