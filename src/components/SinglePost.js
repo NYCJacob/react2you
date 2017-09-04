276f26e38d9d402db919ae6bb319e7305687feaa
Jacob Sherman <jacob@jbsherman.com>
2017-09-04 09:11:54 -0400

    fea: singlepost component displays details on click of listing title

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -14,11 +14,30 @@ class SinglePost extends Component {
     render() {
         return (
             <div className="singlePost-view">
+                <h2> single post componenet </h2>
                 {
                     <div>
-                        <p>single post details</p>
+                        <table className="table table-sm table-responsive" key= {this.props.id}>
+                        <thead>
+                        <tr>
+                        <th className="post-author">By: {this.props.author}</th>
+                        <th className="post-category">Category: {this.props.category}</th>
+                        <th className="post-votes">Votes: {this.props.voteScore}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr><td colSpan={3} className="post-title">{this.props.title}</td></tr>
+                        <tr><td colSpan={3} className="post-body">{this.props.body}</td></tr>
+                        </tbody>
+                        <tfoot>
+                        <tr className="table-info">
+                        <td colSpan={2}>{this.props.id}</td><td>{this.props.timestamp}</td>
+                        </tr>
+                        </tfoot>
+                        </table>
+                    <Comments postId={this.props.id} />
                     </div>
-
+                    
                 }
             </div>
         )
@@ -28,8 +47,10 @@ class SinglePost extends Component {
 
 function mapStateToProps(state) {
     console.log(state);
-
-    return state
+    // state.posts.openTarget
+    let targetPost =   state.posts.items.find((item) => item.id === state.posts.openTarget)
+    console.log(targetPost);
+    return targetPost
 }
 
 export default connect(mapStateToProps)(SinglePost);
