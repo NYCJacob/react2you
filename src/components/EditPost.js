fc9146846212c888516f88bb874de4105ca55188
Jacob Sherman <jacob@jbsherman.com>
2017-09-05 14:58:53 -0400

    bug: redux form not mapping intial state

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -11,28 +11,27 @@ import { Field, reduxForm } from 'redux-form'
  */
 class EditPost extends Component {
 
+    submit = ( values ) => {
+        console.log( values );
+    }
 
     render() {
         const { title, author, category, voteScore, body, id, timestamp } = this.props.postData;
-        const { handleSubmit } = this.props;
+        // const { handleSubmit } = this.props;
 
         return (
 
             <div className="editPost-view">
                 { this.props.editable &&
                     <div>
-                        <form onSubmit={ handleSubmit }>
+                        <form onSubmit={ this.submit }>
                             <div>
-                                <label htmlFor="firstName">First Name</label>
+                                <label htmlFor="firstName">Post Title</label>
                                 <Field name="firstName" component="input" type="text" />
                             </div>
                             <div>
-                                <label htmlFor="lastName">Last Name</label>
+                                <label htmlFor="lastName">Post Body</label>
                                 <Field name="lastName" component="input" type="text" />
-                            </div>
-                            <div>
-                                <label htmlFor="email">Email</label>
-                                <Field name="email" component="input" type="email" />
                             </div>
                             <button type="submit">Submit</button>
                         </form>
@@ -46,6 +45,7 @@ class EditPost extends Component {
 
 function mapStateToProps(state) {
     console.log(state);
+    // let postToEdit = state.posts.items.find()
     return { editable : state.posts.editing }
 }
 
@@ -59,5 +59,15 @@ EditPost = reduxForm({
     // a unique name for the form
     form: 'EditPostForm'
 })(EditPost)
+
+//  redux form example init from state
+// You have to connect() to any reducers that you wish to connect to yourself
+// InitializeFromStateForm = connect(
+//     state => ({
+//         initialValues: state.account.data // pull initial values from account reducer
+//     }),
+//     { load: loadAccount } // bind account loading action creator
+// )(InitializeFromStateForm)
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditPost);
