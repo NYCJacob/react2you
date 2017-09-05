4c0f95a4e921dcb00f1c6d4eaad40eddfffb0608
Jacob Sherman <jacob@jbsherman.com>
2017-09-05 13:52:27 -0400

    fea: basic redux-form renders in EditPost component

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -22,9 +22,20 @@ class EditPost extends Component {
                 { this.props.editable &&
                     <div>
                         <form onSubmit={ handleSubmit }>
-                            { /* form body*/ }
+                            <div>
+                                <label htmlFor="firstName">First Name</label>
+                                <Field name="firstName" component="input" type="text" />
+                            </div>
+                            <div>
+                                <label htmlFor="lastName">Last Name</label>
+                                <Field name="lastName" component="input" type="text" />
+                            </div>
+                            <div>
+                                <label htmlFor="email">Email</label>
+                                <Field name="email" component="input" type="email" />
+                            </div>
+                            <button type="submit">Submit</button>
                         </form>
-
                     </div>
                 }
             </div>
